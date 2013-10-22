656b0d78f3c5c0d2a964d14d453aadebb3aee726
mkiestmcfarland <megan@nird.us>
2013-10-22 12:52:48 -0700

    step 2

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -6,6 +6,18 @@ module Sparsify
     flatten_level(source,'')
   end
 
+  def self.unsparse(source)
+    n = {}
+    source.each do |key, value|
+      if key.include?('.')
+        unflatten_key(key, value, n)
+      else
+        n[key] = value
+      end
+    end
+    return n
+  end
+
   private
   def self.flatten_level(source, parent)
     flattened_hash = {}
@@ -17,5 +29,20 @@ module Sparsify
       end
     end
     return flattened_hash
+  end
+
+  def self.unflatten_key(key, value, n)
+    current_level = n
+    split = key.split('.')
+    split.each_with_index do |level, i|
+      if i == split.length - 1
+        current_level[level] = value
+      else
+        if !current_level.has_key?(level)
+          current_level[level]  = {}
+        end
+        current_level = current_level[level]
+      end
+    end
   end
 end
