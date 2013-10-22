bccc40930d277cfbaa29836d056fcd1421d8b5da
mkiestmcfarland <megan@nird.us>
2013-10-22 12:49:33 -0700

    step 1

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -2,4 +2,20 @@
 
 module Sparsify
   # Your implementation goes here
+  def self.sparse(source)
+    flatten_level(source,'')
+  end
+
+  private
+  def self.flatten_level(source, parent)
+    flattened_hash = {}
+    source.each do |key, value|
+      if value.is_a?(Hash)
+        flattened_hash.merge!(flatten_level(value, parent+key+'.'))
+      else
+        flattened_hash[parent+key] = value
+      end
+    end
+    return flattened_hash
+  end
 end
